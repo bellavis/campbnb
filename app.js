7a0e881bcdb4450287e6032ad787dfd6d5407c69
bell <arbel8@gmail.com>
2021-01-07 20:44:33 +0200

    security update and connecting to mongo atlas

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -5,25 +9,20 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
-
 const methodOverride = require('method-override');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-
-
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const { contentSecurityPolicy } = require('helmet');
+const MongoDBStore = require("connect-mongo")(session);
+const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/campbnb';
 
+
+//mongoose.connect('mongodb://localhost:27017/campbnb'
 mongoose.connect('mongodb://localhost:27017/campbnb', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -46,7 +45,66 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(helmet({contentSecurityPolicy: false}));
+app.use(helmet());
+
+//helmet security policy, allow content (scripts, styles, images..) only froms those sites:
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+];
+const styleSrcUrls = [
+    "https://cdn.jsdelivr.net/",
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dmekt7cps/", //my cloudinary
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+//end of helmet policy -----
+
+
+const store = new MongoDBStore({
+    //dbURL (url)
+    secret: 'thisshouldbeabettersecret!',
+    url: 'mongodb://localhost:27017/campbnb'
+    touchAfter: 24 * 60 * 60
+    });
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
